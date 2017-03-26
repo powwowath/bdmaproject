fd16721a2db496acd05dffe27a65d3d591c104b2
Gerard <ath@athzone.com>
2017-03-26 18:25:57 +0200

    Node and Relations types

--- src/main/java/CreateGraph.java
+++ src/main/java/CreateGraph.java
@@ -4,6 +4,9 @@
  * @author Gerard
  */
 
+import java.io.File;
+import java.util.Random;
+
 import org.apache.commons.io.FileUtils;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Label;
@@ -15,5 +18,17 @@ import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 
 public class CreateGraph {
+
+	private final static File DB = new File("/home/bdma09/data/neo4j/graph.db");
+	
+	private static Random random = new Random();
+
+	enum NodeType implements Label {
+		 City, Poi
+	}
+	
+	enum RelationTypes implements RelationshipType {
+	     flights, has
+	}
 
 }

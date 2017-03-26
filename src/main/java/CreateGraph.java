92ffa342182100383ddedfad26419277c27d5fa5
Gerard <ath@athzone.com>
2017-03-26 10:59:17 +0200

    Initial commit

---
+++ src/main/java/CreateGraph.java
@@ -0,0 +1,19 @@
+/**
+ * Create initial Graph
+ * 
+ * @author Gerard
+ */
+
+import org.apache.commons.io.FileUtils;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
+
+public class CreateGraph {
+
+}

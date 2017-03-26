f4b7510150f9a1e82cf7639be6a8d118f3ca159f
Gerard <ath@athzone.com>
2017-03-26 22:51:10 +0200

    Remove comments

--- src/main/java/edu/upc/bdma/project/CreateGraph.java
+++ src/main/java/edu/upc/bdma/project/CreateGraph.java
@@ -38,20 +38,9 @@ public class CreateGraph {
 	private static final String GRAPH_DB_DATA_FILE = "graph.db";
 	private static final String AIRPORT_FILE = "airports.dat.txt";
 	private static final String ROUTE_FILE = "routes.dat.txt";
-
-/*
- 	private static final ROUTE_FILE = "";
-	private static final ROUTE_FILE = "";
-	private static final ROUTE_FILE = "";
-*/	
 	
-	enum NodeType implements Label {
-		 Airport, Poi
-	}
-	
-	enum RelationTypes implements RelationshipType {
-	     flights, has
-	}
+	enum NodeType implements Label { Airport, Poi }
+	enum RelationTypes implements RelationshipType { flights, has }
 
 	/*
 	 * Main
@@ -66,7 +55,6 @@ public class CreateGraph {
 	 */
 	public static void generate() {
 		File DB = new File(DATA_FOLDER + GRAPH_DB_DATA_FILE);
-		Random random = new Random();
 
 		GraphDatabaseService graph = null;
 		Transaction tx = null;
@@ -77,7 +65,8 @@ public class CreateGraph {
 
 			CSVReader reader = new CSVReader(new FileReader(DATA_FOLDER + AIRPORT_FILE));
 			List<String[]> airportsList = reader.readAll();
-
+			reader.close();
+			
 			HashMap<String, Node> airports = new HashMap<String, Node>();
 			
 			for (int i=0; i<airportsList.size(); i++){
@@ -95,122 +84,6 @@ public class CreateGraph {
 				
 //				System.out.println(airportsList.get(i)[2]);
 			}
-/*			
-			HashMap<String, Node> websites = new HashMap<String, Node>();
-			HashMap<String, Node> customers = new HashMap<String, Node>();
-			for (int i = 0; i < nodes; i++) {
-				Node node = null;
-				if (i%3 > 0) {
-					String number = Values.getPhoneNumber();
-					if (!customers.containsKey(number)) {
-						int age = Values.getAge();
-						String phone = Values.getPhone();
-						
-						node = graph.createNode(NodeType.Customer);
-						node.setProperty("id", number);
-						node.setProperty("number", number);
-						node.setProperty("age", age);
-						node.setProperty("phone", phone);
-						
-						statement.addBatch("INSERT INTO customers VALUES("+
-							"'"+number+"', "+
-							"'"+number+"', "+
-							age+", "+
-							"'"+phone+"');");
-
-						customers.put(number, node);
-					}
-				} else {
-					String url = Values.getURL();
-					if (!websites.containsKey(url)) {
-						node = graph.createNode(NodeType.Website);
-						node.setProperty("id", url);
-						node.setProperty("url", url);
-						
-						statement.addBatch("INSERT INTO websites VALUES("+
-							"'"+url+"', "+
-							"'"+url+"');");
-						
-						websites.put(url, node);
-					}
-				}
-			}
-			
-			ArrayList< ArrayList<Node> > families = new ArrayList< ArrayList<Node> >();
-			int nFamilies = random.nextInt(5)+nodes/5;
-			for (int i = 1; i <= nFamilies; i++) {
-				families.add(new ArrayList<Node>());
-			}
-			for (Node customer : customers.values()) {
-				int family = random.nextInt(nFamilies);
-				families.get(family).add(customer);
-			}
-			for (ArrayList<Node> family : families) {
-				for (int i = 0; i < family.size(); i++) {
-					int edges = random.nextInt(family.size()*3);
-					for (int j = 1; j <= edges; j++) {
-						int action = random.nextInt(2);
-						int edgeTo = random.nextInt(family.size());
-						if (i != edgeTo) {
-							if (action == 0) {
-								double price = Values.getCallPrice();
-								double duration = Values.getCallDuration();
-								String day = Values.getDay();
-								
-								Relationship call = family.get(i).createRelationshipTo(family.get(edgeTo), RelationTypes.calls);
-								call.setProperty("price", price);
-								call.setProperty("duration", duration);
-								call.setProperty("day", day);
-
-								statement.addBatch("INSERT INTO calls VALUES("+
-									"'"+family.get(i).getProperty("number")+"', "+
-									"'"+family.get(edgeTo).getProperty("number")+"', "+
-									price+", "+
-									duration+", "+
-									"'"+day+"\');");
-							} else {
-								double price = Values.getTextPrice();
-								double length = Values.getTextLength();
-								String day = Values.getDay();
-								
-								Relationship text = family.get(i).createRelationshipTo(family.get(edgeTo), RelationTypes.texts);
-								text.setProperty("price", price);
-								text.setProperty("length", length);
-								text.setProperty("day", day);
-								
-								statement.addBatch("INSERT INTO texts VALUES("+
-									"'"+family.get(i).getProperty("number")+"', "+
-									"'"+family.get(edgeTo).getProperty("number")+"', "+
-									price+", "+
-									length+", "+
-									"'"+day+"');");
-							}
-						}
-					}
-					edges = random.nextInt(nodes/5);
-					for (int j = 1; j <= edges; j++) {
-						int edgeTo = random.nextInt(websites.size());
-						for (Node website : websites.values()) {
-							if (edgeTo == 0) {
-								String day = Values.getDay();
-								
-								Relationship visit = family.get(i).createRelationshipTo(website, RelationTypes.visits);
-								visit.setProperty("day", day);
-								
-								statement.addBatch("INSERT INTO visits VALUES("+
-									"'"+family.get(i).getProperty("number")+"', "+
-									"'"+website.getProperty("url")+"', "+
-									"'"+Values.getDay()+"');");
-								break;
-							} else {
-								edgeTo--;
-							}
-						}
-					}
-				}
-			}
-			statement.executeBatch();
-			*/
 			tx.success();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block

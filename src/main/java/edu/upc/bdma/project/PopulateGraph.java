f9082085bb248028de8d53279ae90ae64f17cde1
Gerard <ath@athzone.com>
2017-05-01 21:43:25 +0200

    POIS loading commented Filter process added + csv filtered

---
+++ src/main/java/edu/upc/bdma/project/PopulateGraph.java
@@ -0,0 +1,16 @@
+/**
+ * Create initial Graph
+ *
+ * @author Gerard
+ */
+package edu.upc.bdma.project;
+
+public class PopulateGraph {
+    /*
+     * Main
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
